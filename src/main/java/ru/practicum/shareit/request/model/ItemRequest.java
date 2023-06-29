46b859ca201a09f02c258a69398d86090b053b2d
Mariam <mariam_agadzhanyan@mail.ru>
2023-06-29 20:27:50 +0300

    delete unused imports

--- src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.request.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NonNull;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
