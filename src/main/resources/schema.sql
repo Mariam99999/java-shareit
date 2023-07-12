cf906ba246b9a096e9e523a672263748889bab15
Mariam <mariam_agadzhanyan@mail.ru>
2023-07-12 18:31:52 +0300

    add hibernate

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,25 @@
+
+
+-- create table if  users
+-- (
+--     id    bigint,
+--     name  varchar not null,
+--     email varchar not null,
+--     constraint USERS_PK
+--         primary key (id)
+-- );
+
+-- create unique index USERS_EMAIL_UINDEX
+--     on users (email);
+-- create table items
+-- (
+--     id          bigint,
+--     name        varchar not null,
+--     description varchar not null,
+--     available   boolean not null,
+--     owner_id    bigint  not null,
+--     constraint ITEMS_PK
+--         primary key (id)
+-- );
+
+
