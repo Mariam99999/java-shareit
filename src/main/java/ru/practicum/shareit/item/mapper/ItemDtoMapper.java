78fa278446b36ca6ed9bd5cad54929e7ae525679
Mariam <mariam_agadzhanyan@mail.ru>
2023-07-19 18:53:30 +0300

    Add Bookings

--- src/main/java/ru/practicum/shareit/item/mapper/ItemDtoMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemDtoMapper.java
@@ -2,16 +2,20 @@ package ru.practicum.shareit.item.mapper;
 
 
 import org.springframework.stereotype.Component;
+import ru.practicum.shareit.booking.dto.BookingDtoWithBookerId;
 import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
+import java.util.List;
+
 @Component
 public class ItemDtoMapper {
 
-    public ItemDto mapToDto(Item model, Booking lastBooking, Booking nextBooking) {
-        return new ItemDto(model.getId(), model.getName(), model.getDescription(), model.getAvailable(), lastBooking, nextBooking);
+    public ItemDto mapToDto(Item model, BookingDtoWithBookerId lastBooking, BookingDtoWithBookerId nextBooking, List<CommentDto> comments) {
+        return new ItemDto(model.getId(), model.getName(), model.getDescription(), model.getAvailable(), lastBooking, nextBooking,comments);
     }
 
     public Item mapFromDto(ItemDto model, User owner) {
